fc201c5f531559f430700a4e09d342a3d29560ae
jeanp20201 <jean.paucar2094@gmail.com>
2025-02-26 10:18:35 -0500

    Update BD.sql

--- BD.sql
+++ BD.sql
@@ -50,10 +50,8 @@ INSERT INTO s_cliente VALUES ('202', 'Deportivo Caracas', '58-28066222','31 Saba
 'Caracas', NULL, 'Venezuela', NULL,'EXCELENTE', '12', '2', NULL);
 INSERT INTO s_cliente VALUES ('203', 'New Delhi Sports', '91-11903338','11368 Chanakya',
 'New Delhi', NULL, 'India', NULL,'BUENO', '11', '4', NULL);
-INSERT INTO s_cliente VALUES ('204', 'Ladysport', '1-206-104-0111','281 Queen Street',
-'Seattle', 'Washington', 'US', NULL,'EXCELENTE', '11', '1', NULL);
-INSERT INTO s_cliente VALUES ('205', 'Kim', '852-3693888','15 Henessey Road',
-'Hong Kong', NULL, NULL, NULL,'EXCELENTE', '11', '4', NULL);
+INSERT INTO s_cliente VALUES ('204', 'Ladysport', '1-206-104-0111','281 Queen Street', 'Seattle', 'Washington', 'US', NULL,'EXCELENTE', '11', '1', NULL);
+INSERT INTO s_cliente VALUES ('205', 'Kim', '852-3693888','15 Henessey Road', 'Hong Kong', NULL, NULL, NULL,'EXCELENTE', '11', '4', NULL);
 INSERT INTO s_cliente VALUES ('206', 'Sportique', '33-93425722253','172 Rue de Place',
 'Cannes', NULL, 'France', NULL,'EXCELENTE', '13', '5', NULL);
 INSERT INTO s_cliente VALUES ('207', 'Tall Rock Sports', '234-16036222','10 Saint Antoine',
